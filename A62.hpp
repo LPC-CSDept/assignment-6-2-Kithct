6558de27e78baf76afd0b6d1a24bf14eec48beec
Zelin Chen <6692czl@gmail.com>
2022-10-03 11:41:21 -0700

    Assignment 6-2

--- A62.hpp
+++ A62.hpp
@@ -7,10 +7,18 @@ using namespace std;
 // complete two functions
 double nthpowerp(int n)
 {
+        double nthpower;
+        for(int i=0;i<n;i++)
+                nthpower*=2;
+        cout<<nthpower<<endl;
         // return n_th power number
 }
 double nthpowern(int n)
 {
+        double nthpower;
+        for(int i=0;i<n;i++)
+                nthpower*=0.5;
+        cout<<nthpower<<endl;
         // return n_th power number
 }
 void printout(double result)
