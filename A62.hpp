5739ae3d8cb0d45c649c286d74c4cfa9e7b864da
Zelin Chen <6692czl@gmail.com>
2022-10-03 12:10:36 -0700

    Assignment6-2 (6)

--- A62.hpp
+++ A62.hpp
@@ -3,25 +3,7 @@
 #include <iomanip>
 #include <iostream>
 using namespace std;
-double nthpowerp(int n);
-double nthpowern(int n);
-void printout(double result);
 
-int main()
-{
-        int n;
-        double result;
-        cout<<"Enter your input\n";
-        cin>>n;
-
-        if(n>0)
-                result=nthpowerp(n);
-        else if(n<0)
-                result=nthpowern(n);
-        else
-                result=1;
-        printout(result);
-}
 // complete two functions
 double nthpowerp(int n)
 {
@@ -33,8 +15,8 @@ double nthpowerp(int n)
 }
 double nthpowern(int n)
 {
-        double nthpower=1;
         n*=-1;
+        double nthpower=1;
         for(int i=0;i<n;i++)
                 nthpower*=0.5;
         cout<<nthpower<<endl;
